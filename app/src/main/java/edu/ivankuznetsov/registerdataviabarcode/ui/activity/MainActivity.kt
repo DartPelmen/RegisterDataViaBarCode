bd8425a00cae982eac4132548f541dbd8b5d4f17
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2023-07-12 17:56:11 +1000

    swipe to delete, add excel, add concrete data, add error messages, add paddings

--- app/src/main/java/edu/ivankuznetsov/registerdataviabarcode/ui/activity/MainActivity.kt
+++ app/src/main/java/edu/ivankuznetsov/registerdataviabarcode/ui/activity/MainActivity.kt
@@ -6,18 +6,20 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import edu.ivankuznetsov.registerdataviabarcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     private val cameraContract = registerForActivityResult(ActivityResultContracts.RequestPermission()){
         if(it){
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
-            //контроллер для работы навигации между экранами
-            val navController =
+
+                val navController =
                 supportFragmentManager.findFragmentById(binding.fragmentContainerView.id)
                     ?.findNavController()
             //привязка навигации между экранами к bottom navigation.
